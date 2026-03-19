85d5d80c5f8bcd95b7fff8d1ded1181c540989dd
Harry Dinh <harrydinh1207@gmail.com>
2026-03-19 17:00:30 -0400

    1.0 Beta (2603191640B) (#3)
    * DREAM-1: Set up project directories (#1)
    
    * DREAM-3: Create dream entry model (#2)

--- Dreamie/Dreamie/Views/DreamieApp.swift
+++ Dreamie/Dreamie/Views/DreamieApp.swift
@@ -6,12 +6,18 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct DreamieApp: App {
+    private let containerModels: [any PersistentModel.Type] = [
+        DreamEntry.self
+    ]
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+        .modelContainer(for: containerModels)
     }
 }

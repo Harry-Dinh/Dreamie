6ea4355d33e18bab901aebea6e71457e0e367a35
Harry Dinh <harrydinh1207@gmail.com>
2026-03-19 16:35:24 -0400

    Initial commit
    Created project

---
+++ Dreamie/Dreamie/DreamieApp.swift
@@ -0,0 +1,17 @@
+//
+//  DreamieApp.swift
+//  Dreamie
+//
+//  Created by Harry Dinh on 2026-03-19.
+//
+
+import SwiftUI
+
+@main
+struct DreamieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
